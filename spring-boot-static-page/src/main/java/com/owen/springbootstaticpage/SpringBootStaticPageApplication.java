ae44099ee44dd27c1567adbdeb35e3de993fbb8a
pg-dpw <1632752770@qq.com>
2021-10-04 15:39:59 +0800

    Initial commit

---
+++ spring-boot-static-page/src/main/java/com/owen/springbootstaticpage/SpringBootStaticPageApplication.java
@@ -0,0 +1,13 @@
+package com.owen.springbootstaticpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStaticPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStaticPageApplication.class, args);
+    }
+
+}
---
+++ spring-boot-static-page/src/test/java/com/owen/springbootstaticpage/SpringBootStaticPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.springbootstaticpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStaticPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
